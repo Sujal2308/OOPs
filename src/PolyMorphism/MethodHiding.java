0cbfdb4d8a92e4c6016e899ac54a0d9b210192f1
Sujal2308 <sujalbhugul08@gmail.com>
2025-05-10 01:26:08 +0530

    Method Hiding

---
+++ src/PolyMorphism/MethodHiding.java
@@ -0,0 +1,37 @@
+package PolyMorphism;
+
+public class MethodHiding {
+    /*
+     Take very special care : Static methods can't be overridden as
+     non-static and vice versa but what if both are static ???
+     Then it's not overriding but Method-Hiding
+     */
+
+    /*
+    Note :
+    1) Method Hiding : Method resolution is taken care by compiler
+    based on reference type
+
+    2) Method Overriding : Here method resolution is taken care by
+    JVM based on runtime object type
+     */
+    public static void main(String[] args) {
+        MethodHiding.m1();
+        C5.m1();
+
+        MethodHiding MH = new C5();
+        MH.m1();
+
+    }
+    static void m1(){
+        System.out.println("Parent static method");
+    }
+}
+class C5 extends MethodHiding{
+//    void m1(){     can't override static methods
+//        System.out.println("Child method");
+//    }
+    static void m1(){
+        System.out.println("Child static method");
+    }
+}
