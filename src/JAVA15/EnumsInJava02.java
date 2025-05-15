f94d915d9f3b4ae652add479b1ebb585bf31240b
Sujal2308 <sujalbhugul08@gmail.com>
2025-05-15 20:10:30 +0530

    enums inside a class and method

---
+++ src/JAVA15/EnumsInJava02.java
@@ -0,0 +1,19 @@
+package JAVA15;
+
+public class EnumsInJava02 {
+    // we can happily declare enums inside a class
+    public enum Months{
+        JAN,FEB,MAR,APR,MAY,JUN
+    }
+
+    public static void main(String[] args) {
+        Months JAN = Months.JAN;
+        System.out.println(JAN);
+
+
+        enum DAY{ // Local enum : accessible within the method scope only
+            SUNDAY,MONDAY,TUESDAY;
+        }
+        Day Sunday = Day.SUNDAY;
+    }
+}
