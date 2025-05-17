cc1bdbe80a5fdb9ef7cda670457ea95d408409fc
Sujal2308 <sujalbhugul08@gmail.com>
2025-05-17 23:53:55 +0530

    enums insights

--- src/JAVA15/Enums/EnumsInJava04.java
+++ src/JAVA15/Enums/EnumsInJava04.java
@@ -3,11 +3,17 @@ package JAVA15.Enums;
 public class EnumsInJava04 {
     public static void main(String[] args) {
 
-        // Note we can't explicitly create an object of enum
-//        Company c = new COMPANY();
+        //! Note we can't explicitly create an object of enum
+        //? Enums can't be extended since they are implicitly
+        //? declared as final
+        //* All Enums are the direct childs of java.lang.Enums
+        //*class which itself direct child of OG Object Class
+        //  Company c = new COMPANY();
         Company AMAZON = Company.AMAZON;
         System.out.println(AMAZON.turnOver);
         System.out.println(Company.FLIPKART.getTurnOver());
         System.out.println(Company.MERCEDES.turnOver);
     }
 }
+
+
