c0e5fd8e3f5e55f1dec0283b2995f4f5d0c95b51
Sujal2308 <sujalbhugul08@gmail.com>
2025-06-14 22:45:28 +0530

    count vowels and consonants

--- src/CRT25/Assignments/VowelsConsonants.java
+++ src/CRT25/Assignments/VowelsConsonants.java
@@ -4,19 +4,23 @@ public class VowelsConsonants {
 //        System.out.println((int)'a'+ " "+ (int)'z');
 //        System.out.println((int)'A'+ " "+ (int)'Z');
 
-        String str = "aeiou"; // 2 vowels , 4 consonants
+        String str = "AEIOGD F123"; // 2 vowels , 4 consonants
         countVowelsAndConsonants(str);
 
     }
     static void countVowelsAndConsonants(String str){
+        String s = str.toLowerCase();
         int countVowels = 0;
-        int countConsonants;
-        for(char c : str.toCharArray()){
+        int countConsonants= 0;
+        for(char c : s.toCharArray()){
             if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
                 countVowels++;
             }
+            else if(c>='a' && c<='z'){
+                countConsonants++;
+            }
         }
-        countConsonants = str.length()-countVowels;
+//        countConsonants = str.length()-countVowels;
 
         System.out.println("Vowels : "+countVowels);
         System.out.println("Consonants : "+countConsonants);
