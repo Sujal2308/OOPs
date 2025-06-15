39cb5b670345e7e08eaec15c4b3d1b499614f5cc
Sujal2308 <sujalbhugul08@gmail.com>
2025-06-15 19:05:40 +0530

    find length of arr

--- src/CRT25/FindLengthOfArray.java
+++ src/CRT25/FindLengthOfArray.java
@@ -1,7 +1,7 @@
 package CRT25;
 public class FindLengthOfArray {
     public static void main(String[] args){
-        int arr[] = {1,2,3,4,5,6};
+        int arr[] = {1,2,3,4};
         System.out.println("Length :"+len(arr));
 
     }
