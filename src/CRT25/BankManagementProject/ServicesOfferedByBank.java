6476e06ea852d3e2860e457cb44ee00e648687bd
Sujal2308 <sujalbhugul08@gmail.com>
2025-06-16 23:43:10 +0530

    Services offered by bank

---
+++ src/CRT25/BankManagementProject/ServicesOfferedByBank.java
@@ -0,0 +1,16 @@
+package CRT25.BankManagementProject;
+
+import java.util.Scanner;
+
+public class ServicesOfferedByBank {
+      static void servicesOffered(){
+            System.out.println("Click 1️⃣ to open Account");
+            System.out.println("Click 2️⃣ to deposit money");
+            System.out.println("Click 3️⃣ to withdraw money");
+            System.out.println("Click 4️⃣ to check balance");
+            System.out.println("Click 5️⃣ to get Customer Details");
+            System.out.println("Click 0️⃣ to exit the App");
+        }
+
+
+}
