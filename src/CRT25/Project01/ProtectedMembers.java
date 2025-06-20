acb44183a70087c71424e4f3e525be10596f1770
Sujal2308 <sujalbhugul08@gmail.com>
2025-06-20 22:31:16 +0530

    Protected modifier

---
+++ src/CRT25/Project01/ProtectedMembers.java
@@ -0,0 +1,9 @@
+package CRT25.Project01;
+import CRT25.OOPS.AccessModifiers;
+
+public class ProtectedMembers extends AccessModifiers {
+    public static void main(String[] args){
+        ProtectedMembers P = new ProtectedMembers();
+        P.m1();
+    }
+}
