2e857ea0615811056af651cfdf2ba6c1a517043a
Sujal2308 <sujalbhugul08@gmail.com>
2025-06-25 23:44:26 +0530

    LinkedList more op

---
+++ src/Collections/LinkedList01/LinkedList03.java
@@ -0,0 +1,16 @@
+package Collections.LinkedList01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedList03 {
+    public static void main(String[] args){
+        //! Get op: => T.C: O(n)
+
+        LinkedList<Integer> LL = new LinkedList<>(List.of(1,2,3,4,5));
+        System.out.println(LL.get(2));
+
+    }
+}
